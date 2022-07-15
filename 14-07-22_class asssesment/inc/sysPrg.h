54946018d67ab69a0a091b4e4808365682766166
senarti <108251215+senarti@users.noreply.github.com>
2022-07-15 09:20:33 +0530

    Add files via upload

---
+++ 14-07-22_class asssesment/inc/sysPrg.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <csignal>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+using namespace std;
+int pid,i;
---
+++ 14-07-22_class asssesment/sysPrg.cpp
@@ -0,0 +1,51 @@
+
+
+static void func1(int id)
+{      
+
+         cout<<"I am child :"<<endl;
+         for( i=1;i<10;i++)
+          {
+             if(i% 2!=0)  // Printing Odd numbers for Child
+             cout<<"having Odd numbers:"<<i<<" "<<endl;
+//             exit(0);
+          }
+	cout<<"Child Died"<<endl;
+	wait(NULL);
+	cout<<"ID: "<<id<<endl;
+	
+	
+}
+
+
+int main()
+{
+	signal(SIGCHLD, func1);
+	pid = fork();
+	
+	if(pid == 0)
+	{
+		sleep(5);
+      		cout<<"Child"<<endl;
+		
+	}	
+	else
+	{	
+		
+		
+		
+		wait(0);
+		cout<<"Parent"<<endl;
+		
+             for(int j=1;j<10;j++)
+            { 
+               if(j% 2==0)  // Printing even numbers for Parent
+               cout<<"having even numbers:"<<j<<" "<<endl;
+		
+	}
+       }    cout<<"\nParent Dies"<<endl;
+
+
+	return 0;
+}
+
